ccb9251ad0bd98e57c2eb1f0b36346f0b6887f12
FranLsz <franlsz95develop@gmail.com>
2016-03-30 14:03:40 +0000

    Actualizacion

--- SpMaterialPresupuesto/VisualWebPart1/VisualWebPart1UserControl.ascx.cs
+++ SpMaterialPresupuesto/VisualWebPart1/VisualWebPart1UserControl.ascx.cs
@@ -1,14 +1,60 @@
 ﻿using System;
+using System.Data;
 using System.Linq;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Web.UI.WebControls.WebParts;
+using Microsoft.Office.Server.Utilities;
 using Microsoft.SharePoint;
 
 namespace SpMaterialPresupuesto.VisualWebPart1
 {
     public partial class VisualWebPart1UserControl : UserControl
     {
+        DataTable dtable;
+
+        protected override void OnInit(EventArgs e)
+        {
+            base.OnInit(e);
+
+            dtable = new DataTable();
+
+            dtable.Columns.Add("ID");
+            dtable.Columns.Add("Title");
+            dtable.Columns.Add("Peticion");
+            dtable.Columns.Add("Realizado_x0020_por");
+            dtable.Columns.Add("Fecha");
+            dtable.Columns.Add("Importe", typeof(Decimal));
+        }
+
+        private void ProcessItem(SPListItem item)
+        {
+            string uniqueId = item["ID"].ToString();
+            string title = item["Title"].ToString();
+            string peticion = item["Peticion"].ToString();
+            string realizada;
+            try
+            {
+                realizada = item["Realizado_x0020_por"].ToString();
+            }
+            catch (Exception e)
+            {
+                realizada = "Fran";
+            }
+            string fecha;
+            try
+            {
+                fecha = item["Fecha"].ToString();
+
+            }
+            catch (Exception e)
+            {
+                fecha = "01/01/2016";
+            }
+            decimal importe = Decimal.Parse(item["Importe"].ToString());
+            dtable.Rows.Add(uniqueId, title, peticion, realizada, fecha, importe);
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
         }
@@ -36,10 +82,20 @@ namespace SpMaterialPresupuesto.VisualWebPart1
 ";
             var web = SPContext.Current.Web;
             var list = web.Lists["PresupuestoMaterial"];
-            var items = list.GetItems(query);
-            var dt = items.GetDataTable();
-            lstExpenses.DataSource = dt;
+            //var items = list.GetItems(query);
+            //var dt = items.GetDataTable();
+            //lstExpenses.DataSource = dt;
+            //lstExpenses.DataBind();
+
+            ContentIterator iterator = new ContentIterator();
+            iterator.ProcessListItems(list, query, ProcessItem, ProcessError);
+            lstExpenses.DataSource = dtable;
             lstExpenses.DataBind();
+        }
+
+        private bool ProcessError(SPListItem item, Exception e)
+        {
+            throw new NotImplementedException();
         }
 
         private static bool IsChecked(ListViewDataItem item)
